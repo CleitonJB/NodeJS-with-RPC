80645bdadf0244333d155be433b72548e32861a8
Cleiton Braga <cleitonbraga56@gmail.com>
2023-03-07 17:12:50 -0300

    Fix logic structure to send and get params

--- client-2/get_notes.js
+++ client-2/get_notes.js
@@ -2,10 +2,26 @@
 //* Chamando o metodo List RPC e retornando os dados na console
 
 const client = require('./client-grpc')
-client.list({}, (error, notes) => {
+const prompt = require('prompt-sync')();
+
+var valor = prompt('Valor (reais) para ser investido por 6 meses: ');
+var juros = prompt('Percentual de juros mensal: ');
+
+console.log(`Montante: ${valor}\n`);
+console.log(`Juros: ${juros}\n`);
+
+// client.list({}, (error, notes) => {
+//     if (!error) {
+//         console.log(notes)
+//     } else {
+//         console.error(error)
+//     }
+// });
+
+client.calcular({valor, juros}, (error, notes) => {
     if (!error) {
         console.log(notes)
     } else {
         console.error(error)
     }
-})
+});
--- server/app.js
+++ server/app.js
@@ -1,18 +1,28 @@
-const grpc = require('grpc')
-const notesProto = grpc.load('../server/notes.proto')
+const grpc = require('grpc');
+const notesProto = grpc.load('../server/notes.proto');
+
 const notes = [
     { id: '1', title: 'Note 1', content: 'Content 1' },
     { id: '2', title: 'Note 2', content: 'Content 2' }
-]
+];
+
 const server = new grpc.Server();
+
 server.addService(notesProto.NoteService.service, {
     list: (_, callback) => {
         callback(null, notes)
     },
-})
-server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure())
-console.log('Server running at http://127.0.0.1:50051')
-server.start()
+    calcular: (_, callback) => {
+        console.log("Calculando papai");
+        console.log(_.request);
+
+        callback(null, { resultado: {valor: '1', juros: '2', meses: '3', jurosComposto: '4', montante: '5'}})
+    },
+});
+
+server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
+console.log('Servidor rodando em http://127.0.0.1:50051');
+server.start();
 
 //* Fontes
 //  - https://www.mundojs.com.br/2020/07/31/introducao-ao-grpc-com-node-js/
